a2f30eef2d31405286a9498dcc3e60a441c7becf
Jin <jinson@uw.edu>
2021-04-15 08:24:57 -0700

    finished the program

---
+++ app/src/main/java/com/example/dotify/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvPlayCount: TextView
+    private lateinit var btnChangeUser: Button
+    private val randomNum = Random.nextInt(10, 50)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvPlayCount = findViewById(R.id.tvPlayCount)
+        tvPlayCount.text = randomNum.toString()
+
+        btnChangeUser = findViewById(R.id.btnChangeUser)
+
+
+        btnChangeUser.setOnClickListener {
+            val btnText = btnChangeUser.text.toString()
+            changeUserClicked(btnText)
+        }
+
+        val imgBtnNext = findViewById<ImageButton>(R.id.imgBtnNext)
+
+        imgBtnNext.setOnClickListener { nextClicked() }
+
+
+    }
+
+    fun nextClicked() {
+        Toast.makeText(this, "Skipping to next Track", Toast.LENGTH_SHORT).show()
+    }
+
+    fun prvClicked(view: View) {
+        Toast.makeText(this, "Skipping to previous Track", Toast.LENGTH_SHORT).show()
+    }
+
+    fun playClicked(view: View) {
+        var curNum = tvPlayCount.text.toString().toInt()
+        curNum++
+        var curNumString = curNum.toString()
+        tvPlayCount.text = "$curNumString plays"
+    }
+
+    fun changeUserClicked(btnText: String) {
+        Log.i("jin", btnText)
+        val tvUserName = findViewById<TextView>(R.id.tvUserName)
+        val etUserName = findViewById<EditText>(R.id.etUserName)
+
+        // prompt input from the user
+        if (btnText == "Change User") {
+            tvUserName.visibility = View.GONE
+            etUserName.visibility = View.VISIBLE
+            btnChangeUser.text = "Apply"
+
+        } else if (btnText == "Apply") {
+            tvUserName.visibility = View.VISIBLE
+            etUserName.visibility = View.GONE
+            var input = etUserName.text.toString()
+            tvUserName.text = input
+            btnChangeUser.text = "Change User"
+        }
+    }
+
+
+}
