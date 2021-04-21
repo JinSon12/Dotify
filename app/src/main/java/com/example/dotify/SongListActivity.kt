fc0aca6d5ded1e71d8ad3922ce5bd6a972cfe5cf
Jin <jinson@uw.edu>
2021-04-21 05:40:43 -0700

    song shuffle button working

--- app/src/main/java/com/example/dotify/SongListActivity.kt
+++ app/src/main/java/com/example/dotify/SongListActivity.kt
@@ -2,10 +2,35 @@ package com.example.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.ericchee.songdataprovider.Song
+import com.ericchee.songdataprovider.SongDataProvider
+import com.example.dotify.databinding.ActivitySongListBinding
 
 class SongListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_list)
+
+        this.setTitle("All Songs")
+
+        val listofSong: List<Song> = SongDataProvider.getAllSongs()
+
+        val binding = ActivitySongListBinding.inflate(layoutInflater).apply { setContentView(root)}
+
+
+
+        with(binding) {
+            val adapter = SongListAdapter(listofSong)
+
+            // the recyclerView's adapter should be PeopleAdapter(people) or val adapter
+            rvSongList.adapter = adapter
+
+            btnRefresh.setOnClickListener {
+                adapter.updateSongList(listofSong.toMutableList().shuffled())
+
+            }
+        }
+
     }
 }
